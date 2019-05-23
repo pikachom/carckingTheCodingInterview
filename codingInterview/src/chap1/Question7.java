f17f13bb9cf86dcaeb6c0af51dce6e2a6a971611
kihun_um <kihun_um@tmax.co.kr>
2019-05-23 17:54:25 +0900

    gg

--- codingInterview/src/chap1/Question7.java
+++ codingInterview/src/chap1/Question7.java
@@ -8,34 +8,23 @@ public class Question7 {
 		return false;
 	}
 	public int[][] rotator(int[][] matrix){
-		int n = matrix[0].length; // n=4
-		int[] tmp = new int[n-1];
-		int[] top = new int[n-1];
-		int[] right = new int[n-1];
-		int[] bottom = new int[n-1];
-		int[] left = new int[n-1]; // 더 나은 방법이 있나??
-				
-		for(int i=0;i<n-1;i++) { // 각 껍데기 값을 집어넣음
-			top[i] = matrix[0][i];
-			bottom[i] = matrix[n-1][n-1-i];
-			right[i] = matrix[i][n-1];
-			left[i] = matrix[n-1-i][0];			
-		}
-		tmp = right;
-		right = top;
-		top = left;
-		left = bottom;
-		bottom = tmp; 
+		int len = matrix.length;
+		int lastIdx = len-1;
 		
-		for(int i=0;i<n-1;i++) { // 각 껍데기 값을 집어넣음
-			matrix[0][i] = top[i];
-			matrix[n-1][n-1-i] = bottom[i];
-			matrix[i][n-1] = right[i];
-			matrix[n-1-i][0] = left[i];			
-		}				
-	
-		return matrix;	
-	}
+		
+		 for (int i = 0; i < len / 2; i++) {
+		        for (int j = 0; j < lastIdx - 2 * i; j++) {
+		            int tmp = matrix[j + i][lastIdx - i];
+		            matrix[j + i][lastIdx - i] = matrix[i][j + i];
+		            matrix[i][j + i] = matrix[lastIdx - j - i][i];
+		            matrix[lastIdx - j - i][i] = matrix[lastIdx - i][lastIdx - j - i];
+		            matrix[lastIdx - i][lastIdx - j - i] = tmp;
+		        }
+		    }
+		 return matrix;
+		}
+	        
+
 	public void printMatrix(int[][] matrix) {
 		int n = matrix[0].length;
 		for(int i=0;i<n;i++) {
@@ -49,8 +38,9 @@ public class Question7 {
 	public static void main(String[] args) {
 		int[][] matrix = new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
 		Question7 test = new Question7();
-		System.out.println(test.isRotatable(matrix));
-		System.out.println(test.rotator(matrix));
+		
+		test.printMatrix(matrix);
+		System.out.println("====================");
 		test.printMatrix(test.rotator(matrix));
 	}
 }
