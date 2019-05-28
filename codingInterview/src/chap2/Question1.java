b9c8391a2d6d12f293f04a9580734409e1ae7d02
kihun_um <kihun_um@tmax.co.kr>
2019-05-28 18:03:15 +0900

    q1 존나 안되네 ㅆㅃ꺼

--- codingInterview/src/chap2/Question1.java
+++ codingInterview/src/chap2/Question1.java
@@ -1,8 +1,39 @@
 package chap2;
 
+import java.util.HashSet;
+
 public class Question1 {
-	public void removeDuplication(LinkedListNode link) {
-		
+	public void removeDuplication(LinkedListNode ll) {
+		LinkedListNode.Node n = ll.head;
+		LinkedListNode.Node tmpNode  = null;
+		HashSet<Integer> dataSet = new HashSet<Integer>();
+		while(n.next != null) {			
+			if(dataSet.contains(n.data)) {
+				tmpNode = n;
+				//¾Æ´Ï¾¯¹úµµ´ëÃ¼¿Ö ¾ÈµÅ????
+			}else {
+				dataSet.add(n.data);				
+			}
+			
+			n = n.next;			
+		}
+				
+	}
+	public static void main(String[] args) {
+		LinkedListNode ll = new LinkedListNode();
+		ll.appendToTail(1);
+		ll.appendToTail(2);
+		ll.appendToTail(3);
+		ll.appendToTail(3);
+		ll.appendToTail(4);
+		ll.appendToTail(3);
+		ll.appendToTail(2);
+		ll.appendToTail(1);
+		ll.printList();
+		System.out.println("====================================");
+		Question1 test = new Question1();
+		test.removeDuplication(ll);		
+		ll.printList();
 		
 	}
 }
