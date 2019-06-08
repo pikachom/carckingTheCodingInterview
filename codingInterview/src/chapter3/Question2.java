3ba989474c0eb69e15e23a9bc76100284e50596f
Kh Um <Kh Um@DESKTOP-H1MTIIC>
2019-06-09 01:19:03 +0900

    아니 2번문제 각잡다가 끝냄

--- codingInterview/src/chapter3/Question2.java
+++ codingInterview/src/chapter3/Question2.java
@@ -4,7 +4,7 @@ public class Question2 {
 	public class StackWithMin<NodeWithMin> extends MyStack<NodeWithMin>{
 		public class NodeWithMin<Integer>{
 			public NodeWithMin(Integer data) {
-				//Mystack 띄워놓고 마저 만들자...
+				//Mystack 띄워놓고 마저 만들자...ㅇ
 			}
 		}
 	}
