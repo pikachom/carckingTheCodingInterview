16d44fba8bf9f3695e283b76be10f85a4215c259
kihun_um <kihun_um@tmax.co.kr>
2019-06-11 21:06:24 +0900

    불필요한 오버라이딩 등을 찾아주세요

--- codingInterview/src/chapter3/Question2.java
+++ codingInterview/src/chapter3/Question2.java
@@ -1,12 +1,51 @@
 package chapter3;
 
+import java.util.EmptyStackException;
+
 public class Question2 {
-	public class StackWithMin<NodeWithMin> extends MyStack<NodeWithMin>{
+	public class StackWithMin<Integer> extends MyStack<Integer>{
+				
 		public class NodeWithMin<Integer>{
+			private NodeWithMin<Integer> next;
+			private Integer data;
+
 			public NodeWithMin(Integer data) {
-				//Mystack 띄워놓고 마저 만들자...ㅇ
+				this.data = data;				
 			}
+		}				
+		private NodeWithMin<Integer> top;
+		private MyStack<Integer> minStack;
+		
+		
+		public Integer pop() {
+			if(top == null) throw new EmptyStackException();
+			Integer item = top.data;
+			top = top.next;
+			if((int)item == (int)minStack.peek()) {
+				minStack.pop();
+			}
+			return item;
 		}
+		
+		public void push(Integer data) {			
+			NodeWithMin<Integer> node = new NodeWithMin<Integer>(data);
+			node.next = top;
+			top = node;			
+			if(((int)minStack.peek()>(int)data) || (minStack.isEmpty())) {
+				minStack.push(data);
+			}			
+		}
+		
+		public Integer peek() {
+			if(top == null) throw new EmptyStackException();
+			return top.data;
+		}
+		public Integer min() {
+			if(top == null) throw new EmptyStackException();
+			return minStack.peek();
+		}
+		
+		
 	}
 	
 	
