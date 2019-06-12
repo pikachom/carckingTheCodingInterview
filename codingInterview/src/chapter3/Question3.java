0eb5f74602e3ab5d1e9e1636ad962ea607af795c
kihun_um <kihun_um@tmax.co.kr>
2019-06-12 22:27:40 +0900

    asdf

---
+++ codingInterview/src/chapter3/Question3.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+import java.util.ArrayList;
+
+public class Question3 {
+	public class SetOfStacks{
+		public SetOfStacks(int capacity) {
+			this.capacity = capacity;
+		}
+		public int capacity;
+		
+		ArrayList<MyStack<Integer>> stacks = new ArrayList<MyStack<Integer>>();		
+		
+		public void push(Integer item) {
+			
+		}
+		
+			
+		
+	}
+}
