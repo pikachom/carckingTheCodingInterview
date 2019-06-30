6f6c29fd0cc7730fafbbbe041c7c383e11359cf8
Kh Um <Kh Um@DESKTOP-H1MTIIC>
2019-06-30 19:27:17 +0900

    sdf

--- codingInterview/src/chapter4/Question4.java
+++ codingInterview/src/chapter4/Question4.java
@@ -1,5 +1,22 @@
 package chapter4;
 
 public class Question4 {
-	
+	public int getHeight(TreeNode root) {
+		if(root == null) {
+			return -1;
+		}
+		return Math.max(getHeight(root.left),  getHeight(root.right)) + 1 ; 
+	}
+	public boolean isBalanced(TreeNode root) {
+		if(root == null) {
+			return true;
+		}
+		int heightDiff = Math.abs(getHeight(root.left) - getHeight(root.right));
+		if(heightDiff > 1) {
+			return false;
+		} else {
+			return isBalanced(root.left) && isBalanced(root.right); 
+		}
+		
+	}
 }
