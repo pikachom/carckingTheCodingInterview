2898120fd69b35d2534ab70d7c5ac8a322dd1071
Kihun_Um <pikachom@gmail.com>
2019-07-28 01:47:26 +0900

    working on q3

--- codingInterview/src/chapter7/Question1.java
+++ codingInterview/src/chapter7/Question1.java
@@ -1,5 +1,39 @@
 package chapter7;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Question1 {
 	//deck of card -_-??
+}
+class Deck{
+	ArrayList<Card> cardDeck;
+	public Deck() {
+		this.cardDeck = new ArrayList<Card>();
+		for(Suit suit : Suit.values()) {
+			for(int i=1; i<=12; i++) {
+				Card oneCard = new Card(suit, i);
+				cardDeck.add(oneCard);
+			}
+		}
+	}	
+	public Card getLastCard() {
+		return cardDeck.remove(cardDeck.size());
+	}
+	public void shuffle() {
+		Collections.shuffle(cardDeck);
+	}
+	
+}
+class Card{
+	Suit suit;
+	int faceValue;
+	public Card(Suit s, int f) {
+		this.suit = s;
+		this.faceValue = f;
+	}
+}
+
+enum Suit{
+	Club, Diamond, Heart, Spade;
 }
--- codingInterview/src/chapter7/Question2.java
+++ codingInterview/src/chapter7/Question2.java
@@ -3,53 +3,82 @@ package chapter7;
 import java.util.Queue;
 
 public class Question2 {
-	public class CallCenter{
-		private int respondentNum;
-		private int managerNum;
-		private int directorNum;
-		private employee[] respondents;
-		private employee[] managers;
-		private employee[] directors;
-		private Queue<call> callQueue;
-		public CallCenter(int respondentNum, int managerNum, int directorNum) {
-			this.respondentNum = respondentNum;
-			this.managerNum = managerNum;
-			this.directorNum = directorNum;
-			this.respondents = new employee[respondentNum];
-			this.managers = new employee[managerNum];
-			this.directors = new employee[directorNum];
-			
-		}
-		public void enqueueCall(call c) {
-			this.callQueue.add(c);
-		}
-		public call dequeueCall(){
-			call c = this.callQueue.poll();
-			return c;
-		}
-		public void dispatchCall(call c) {
-			if(c.callRank == 3) {
-				//directors가 꽉찼는지 보고 할당 or 콜큐에 넣
-			}
-			//반복....
-		}
-	}
-	public class employee{
-		Status workerStatus = Status.free;
-	}
-	public class call{
-		private int callRank;
-		public call(int callRank) {
-			this.callRank = callRank;
-		}
-		
-	}
-	enum Status{
-		busy, free;
-	}
 	public static void main(String[] args) {
 		Question2 test = new Question2();
-		Question2.CallCenter cs = new Question2.CallCenter(10,4,2);
+		//Question2.CallCenter cs = new Question2.CallCenter(10,4,2);
 		// 이거 어케함 ㅡㅡ?
 	}
 }
+class CallCenter{
+	private int respondentNum;
+	private int managerNum;
+	private int directorNum;
+	private employee[] respondents;
+	private employee[] managers;
+	private employee[] directors;
+	private Queue<call> callQueue;
+	public CallCenter(int respondentNum, int managerNum, int directorNum) {
+		this.respondentNum = respondentNum;
+		this.managerNum = managerNum;
+		this.directorNum = directorNum;
+		this.respondents = new employee[respondentNum];
+		this.managers = new employee[managerNum];
+		this.directors = new employee[directorNum];
+		
+	}
+	public void enqueueCall(call c) {
+		this.callQueue.add(c);
+	}
+	public call dequeueCall(){
+		call c = this.callQueue.poll();
+		return c;
+	}
+	public void dispatchCall(call c) {
+		if(c.callRank == 3) {
+			
+			//directors가 꽉찼는지 보고 할당 or 콜큐에 넣
+		}else if(c.callRank == 2) {
+			
+		}
+		//반복....
+	}
+	public void directorGetCall(call C) {
+		for(int n=0;n<directors.length;n++) {
+			if(directors[n].workerStatus==Status.free) {
+				directors[n].workerStatus=Status.busy;
+				break;
+			}
+		}			
+	}
+	public void managerGetCall(call C) {
+		for(int n=0;n<managers.length;n++) {
+			if(managers[n].workerStatus==Status.free) {
+				managers[n].workerStatus=Status.busy;
+				break;
+			}
+		}
+	}
+	public void respondentGetCall(call C) {
+		for(int n=0;n<respondents.length;n++) {
+			if(respondents[n].workerStatus==Status.free) {
+				respondents[n].workerStatus=Status.busy;
+				break;
+			}
+		}
+	}
+}
+
+class employee{
+	Status workerStatus = Status.free;
+}
+class call{
+	public int callRank;
+	public call(int callRank) {
+		this.callRank = callRank;
+	}
+	
+}
+enum Status{
+	busy, free;
+}
+	
