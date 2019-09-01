d2acae885dad79c7d809025db527f394fc64d896
Kihun_Um <pikachom@gmail.com>
2019-09-01 19:27:49 +0900

    q5 solution is genius...

--- codingInterview/src/chapter8/Question5.java
+++ codingInterview/src/chapter8/Question5.java
@@ -1,5 +1,30 @@
 package chapter8;
 
 public class Question5 {
-
+	//solution is genius....
+	public int getProduct(int a, int b) {
+		if(a>b) {
+			int temp = a;
+			a=b;
+			b=temp;
+		}
+		
+		return recurProduct(a, b);
+	}
+	private int recurProduct(int smaller, int bigger) {
+		if(smaller == 0) {
+			return 0;
+		}else if(smaller == 1) {
+			return bigger;
+		}
+		
+		int halfOfSmaller = smaller>>1;
+		int halfResult = recurProduct(halfOfSmaller, bigger);
+		
+		if(smaller%2 == 0) {
+			return halfResult + halfResult;
+		}else {
+			return halfResult + halfResult + bigger;
+		}
+	}
 }
