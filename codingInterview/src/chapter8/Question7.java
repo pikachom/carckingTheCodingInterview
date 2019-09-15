7be18dfda4cae268dae22a64ff82487f0bfebe78
Kihun_Um <pikachom@gmail.com>
2019-09-16 01:00:30 +0900

    ahahah

--- codingInterview/src/chapter8/Question7.java
+++ codingInterview/src/chapter8/Question7.java
@@ -1,5 +1,40 @@
 package chapter8;
 
-public class Question7 {
+import java.util.HashSet;
 
+public class Question7 {
+	public HashSet<String> permutationWithoutDup(String input){
+		HashSet<String> result = new HashSet<String>();
+		if(input==null) {
+			return null;
+		}
+		if(input.length()==0) {
+			result.add("");
+			return result;
+		}
+		String recurInput = input.substring(0, input.length()-1);
+		String lastChar = input.substring(input.length()-1);
+		
+		HashSet<String> recurResult = permutationWithoutDup(recurInput);
+		for(String recurPermutation : recurResult) {
+			for(int i=0; i<=recurPermutation.length(); i++) {
+				String front = recurPermutation.substring(0, i);
+				String back = recurPermutation.substring(i);
+				String permutation = front + lastChar + back;
+				result.add(permutation);
+			}
+		}
+		
+		
+		
+		return result;
+		
+	}
+	
+	public static void main(String[] args) {
+		Question7 test = new Question7();
+		System.out.println(test.permutationWithoutDup("abcde"));
+//		System.out.println("abcde".substring(0, 4));
+//		System.out.println("abcde".substring(4));
+	}
 }
