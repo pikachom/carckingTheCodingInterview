06706c0d38902a00df58842cabe7ca64a10e9540
Kihun_Um <pikachom@gmail.com>
2019-11-17 14:17:49 +0900

    mergesort commit

--- codingInterview/src/chapter10/MergeSort.java
+++ codingInterview/src/chapter10/MergeSort.java
@@ -14,8 +14,34 @@ public class MergeSort {
 		}
 	}
 	void merge(int[] array, int[] helper, int low, int middle, int high) {
+		/*copy both halves into a helper array*/
 		for(int i=low; i<=high; i++) {
 			helper[i] = array[i];
+		}
+		
+		int helperLeft = low;
+		int helperRight = middle + 1;
+		int current = low;
+		
+		/* iterate through helper array, compare left<->right half,
+		 * copy smaller element from the two halves into the original array
+		 * */
+		
+		while (helperLeft <= middle && helperRight <= high) {
+			if (helper[helperLeft] <= helper[helperRight]) {
+				array[current] = helper[helperLeft];
+				helperLeft++;
+			} else { //if right element is smaller than left element
+				array[current] = helper[helperRight];
+				helperRight++;
+			}
+			current++;
+		}
+		/* copy the rest of the left side of the array into the target array
+		 * */
+		int remaining = middle - helperLeft;
+		for (int i = 0; i <= remaining; i++) {
+			array[current + i] = helper[helperLeft + i];
 		}
 	}
 }
