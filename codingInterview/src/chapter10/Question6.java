ccca4f4cfa0128c4934a47ee5f4dfbc247c2f766
Kihun_Um <pikachom@gmail.com>
2019-11-24 23:41:42 +0900

    3 problems left and all problems are so difficult

---
+++ codingInterview/src/chapter10/Question6.java
@@ -0,0 +1,14 @@
+package chapter10;
+
+public class Question6 {
+	/* how to sort big file(20GB)?
+	 * 
+	 * divide file into remaining RAM size
+	 * (for example, i have 16GB RAM system, and using 3GB -> 13GB each file)
+	 * and sort each sub-files
+	 * merge
+	 * end!
+	 * 
+	 * 
+	 * */
+}
